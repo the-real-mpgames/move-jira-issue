07eaf0b90c18db48258f1f6b0baf7ecda4486d79
metalsong <ramix.pro@gmail.com>
2020-09-22 17:00:57 +0300

    added for pull request opened automation

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const github = require('@actions/github');
 const request = require('request-promise-native');
 const matchAll = require("match-all");
 const unique = require('array-unique');
+const transitionId = process.env['JIRA_TRANSITION_ID'];
+const buildNumber = process.env['GITHUB_RUN_NUMBER'];
+const body = github.context.payload.pull_request.body
+const title = github.context.payload.pull_request.title
+const littlestring = ' ';
+const bigstring = title + littlestring + body;
 
 try {
     const jiraEmail = process.env['JIRA_EMAIL'];
@@ -10,6 +16,9 @@ try {
     const jiraUrl = process.env['JIRA_URL']
     const action = core.getInput('jira-action');
     switch (action) {
+        case 'move_card_when_pull_request_opened':
+            moveIssueWhenPullRequestOpen(jiraEmail, jiraToken, jiraUrl);
+            break;
         case 'move_issue_when_pull_request_closed':
             moveIssueWhenPullRequestClose(jiraEmail, jiraToken, jiraUrl);
             break;
@@ -18,13 +27,7 @@ try {
     core.setFailed(error.message);
 }
 
-function moveIssueWhenPullRequestClose(jiraEmail, jiraToken, jiraUrl) {
-    const transitionId = process.env['JIRA_TRANSITION_ID'];
-    const buildNumber = process.env['GITHUB_RUN_NUMBER'];
-    const body = github.context.payload.pull_request.body
-    const title = github.context.payload.pull_request.title
-    const littlestring = ' ';
-    const bigstring = title + littlestring + body;
+function moveIssueWhenPullRequestOpen(jiraEmail, jiraToken, jiraUrl) {
     const start = async () => {
         const listOfIds = unique( matchAll(bigstring,  /(LH\-[0-9]{1,5})/g).toArray());
         if (listOfIds.length == 0) {
@@ -38,7 +41,27 @@ function moveIssueWhenPullRequestClose(jiraEmail, jiraToken, jiraUrl) {
         }
         console.log("Found JIRA issue keys: " + listOfIds)
         for (item of listOfIds) {
-            moveIssue(jiraEmail, jiraToken, jiraUrl, item, transitionId, );
+            moveIssue(jiraEmail, jiraToken, jiraUrl, item, transitionId);
+        }
+      }
+      start();     
+}
+
+function moveIssueWhenPullRequestClose(jiraEmail, jiraToken, jiraUrl) {
+    const start = async () => {
+        const listOfIds = unique( matchAll(bigstring,  /(LH\-[0-9]{1,5})/g).toArray());
+        if (listOfIds.length == 0) {
+            console.log("There is no JIRA issue keys in PR title and Body")
+            return;
+        }
+        const pattern = /(wip:)/gi;
+        if (pattern.test(title) == true) {
+            console.log("found WIP: flag in PR title, issues won't be moved To Test")
+            return;
+        }
+        console.log("Found JIRA issue keys: " + listOfIds)
+        for (item of listOfIds) {
+            moveIssue(jiraEmail, jiraToken, jiraUrl, item, transitionId);
             addBuildComment(jiraEmail, jiraToken, jiraUrl, item, buildNumber);
         }
       }
